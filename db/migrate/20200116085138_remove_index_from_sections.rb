03aac5d5c4deb20d6c9bdc625edf266abc4b0130
Abel <abelchiao@gmail.com>
2020-01-17 06:58:17 -0800

    Remove migration file

--- db/migrate/20200116085138_remove_index_from_sections.rb
+++
@@ -1,6 +0,0 @@
-class RemoveIndexFromSections < ActiveRecord::Migration[5.2]
-  def change
-    remove_index :sections, name: "index_sections_on_project_id_and_title"
-    add_index :sections, :project_id
-  end
-end
